1601c0199061d1e9d2e83cf6b53d07aff7bd8814
nathanrodrig <143113149+nathan2005-maker@users.noreply.github.com>
2026-07-16 15:50:29 -0300

    <Rafactor>(cmd/main) Adicionando um ispe32 PE

--- cmd/main.c
+++ cmd/main.c
@@ -1,5 +1,7 @@
 #include "pch.h"
 
+bool ispe32(IMAGE_DOS_HEADER buffer, char* argv[]);
+
 
 int main(int argc, char* argv[]) {
 	FILE* fh = NULL;
@@ -19,6 +21,28 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
+	IMAGE_DOS_HEADER buffer;
+
+	if(fread(&buffer, 32, sizeof(IMAGE_DOS_HEADER),1, fh)!= 1){
+		VTableError_File(TYPE_ERROR_FILE_INVALID,
+			"NOT POSSIBLE TO READ 32 BYTES FROM THE FILE",
+			argv[1]);
+		return EXIT_FAILURE;
+	}
+
 	fclose(fh);
+	return EXIT_SUCCESS;
+}
+
+
+bool ispe32(IMAGE_DOS_HEADER buffer, char* argv[]) {
+	//"PE\0\0" é IMAGE_NT_SIGNATURE (0x00004550)
+	if (buffer.e_magic != IMAGE_DOS_SIGNATURE) {
+		VTableError_File(TYPE_ERROR_FILE_INVALID,
+			"FILE IS NOT A VALID DOS EXECUTABLE",
+			argv[1]);
+		return EXIT_FAILURE;
+	}
+	puts("FILE IS A VALID DOS EXECUTABLE");
 	return EXIT_SUCCESS;
 }
