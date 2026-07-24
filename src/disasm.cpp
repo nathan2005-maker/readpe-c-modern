2c7028d80d2f7bfb8325ebee1e8d8631f238d577
nathanrodrig <143113149+nathan2005-maker@users.noreply.github.com>
2026-07-24 17:59:42 -0300

    <Refactor(CMakeLists) Estrutura do desmontador

--- src/disasm.cpp
+++ src/disasm.cpp
@@ -4,4 +4,19 @@
 #include <cstdio>
  
 extern "C" void
-disasm_print_bytes(const uint8_t* code, size_t size, uint64_t runtime_address)
+disasm_print_bytes(const uint8_t* code, size_t size, uint64_t runtime_address) {
+
+
+    ZydisDecoder decoder;
+    ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_STACK_WIDTH_64);
+
+    ZydisFormatter formatter;
+    ZydisFormatterInit(&formatter, ZYDIS_FORMATTER_STYLE_INTEL);
+
+    ZyanUSize offset = 0;
+    ZydisDecodedInstruction instruction;
+    ZydisDecodedOperand operands[ZYDIS_MAX_OPERAND_COUNT];
+
+
+
+}
